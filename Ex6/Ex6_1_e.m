9a9ed0391374d0f94ab4db4ecebbabf886a915d6
azerty250 <36883459+azerty250@users.noreply.github.com>
2018-05-16 10:27:19 +0200

    Latest update

--- Ex6/Ex6_1_e.m
+++ Ex6/Ex6_1_e.m
@@ -1,27 +1,51 @@
 
+N = 1;
 
 %define the dynamic functions
 gamma = 3;
 alpha = 1.6;
 beta = 0.01;
-U0 = [0, -0.01];
-X0 = [0, 0.5];
-v_dot = @(v) gamma*U0(1) - alpha*U0(2)*v -beta*(v^3);
+U0 = [0; -0.01];
+X0 = [0; 0.5];
+
+u = U0;
+x = X0;
+
+v_dot = @(v,u) gamma*u(1) - alpha*u(2)*v -beta*(v.^3);
 lambda_dot = @(v) v;
 
+h=0.1;   % step size
+  
+f_discrete = X0;
 
+%%
 %Euler forward method
-h=1.5;                                             % step size
-x = 0:h:3;                                         % Calculates upto y(3)
-y = zeros(1,length(x)); 
-y(1) = 5;                                          % initial condition
-F_xy = @(t,r) 3.*exp(-t)-0.4*r;                    % change the function as you desire
-
-for i=1:(length(x)-1)                              % calculation loop
-    k_1 = F_xy(x(i),y(i));
-    k_2 = F_xy(x(i)+0.5*h,y(i)+0.5*h*k_1);
-    k_3 = F_xy((x(i)+0.5*h),(y(i)+0.5*h*k_2));
-    k_4 = F_xy((x(i)+h),(y(i)+k_3*h));
-
-    y(i+1) = y(i) + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h;  % main equation
+for i = 1:N
+    
+    f_discrete(1,i+1) = f_discrete(1,i) + h*v_dot(f_discrete(2,i), u(:,i)); 
+    f_discrete(2,i+1) = f_discrete(1,i) + h*lambda_dot(f_discrete(2,i)); 
+    
 end
+
+f_discrete
+
+%%
+
+%RK4
+for i=1:N                              % calculation loop
+    k_1 = v_dot(f_discrete(2,i), u(:,i));
+    k_2 = v_dot(f_discrete(2,i)+0.5*h*k_1,u(:,i)+0.5*h);
+    k_3 = v_dot((f_discrete(2,i)+0.5*h*k_2),(u(:,i)+0.5*h));
+    k_4 = v_dot((f_discrete(2,i)+k_3*h),(u(:,i)+h));
+
+    f_discrete(2,i+1) = f_discrete(2,i) + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h;  % main equation
+    
+    k_1 = lambda_dot(f_discrete(1,i));
+    k_2 = lambda_dot(f_discrete(1,i)+0.5*h*k_1);
+    k_3 = lambda_dot((f_discrete(1,i)+0.5*h*k_2));
+    k_4 = lambda_dot((f_discrete(1,i)+k_3*h));
+
+    f_discrete(1,i+1) = f_discrete(1,i) + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h;
+end
+
+f_discrete
