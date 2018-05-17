134cfb4db1c4cd504b88a7e8b5cffd76958657db
azerty250 <36883459+azerty250@users.noreply.github.com>
2018-05-17 14:29:55 +0200

    RK4

--- Ex6/template/integrators/RK4.m
+++ Ex6/template/integrators/RK4.m
@@ -14,8 +14,7 @@ function [x_next] = RK4(X,U,h,f)
 
     k_1 = f(X,U);
     k_2 = f(X+0.5*h*k_1,U+0.5*h);
-    k_3 = f(X+0.5*h*k_2),U+0.5*h));
-    k_4 = f((X+k_3*h),U+h));
+    k_3 = f(X+0.5*h*k_2,U+0.5*h);
+    k_4 = f(X+k_3*h,U+h);
 
     x_next = X + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h;  % main equation
-%error('Remove this when you have implemented the function')
